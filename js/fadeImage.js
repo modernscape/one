7469a51f049571755152224e2608471f61e34c2b
modernscape <modernscape@gmail.com>
2022-05-18 14:41:38 +0900

    n

--- js/fadeImage.js
+++ js/fadeImage.js
@@ -1,31 +1,27 @@
-document.addEventListener('DOMContentLoaded',init);
-document.addEventListener('keypress', keypress_ivent);
+document.addEventListener('DOMContentLoaded', init);
+// document.addEventListener('keypress', keypress_ivent);
 var screenDiv;
 var imageIndex = 0;
 var imageNum = 11;
-
-function keypress_ivent(e) {
-	if(e.code === 'Space'){
-        changeImage();
-	}
-	return false; 
-}
- 
-function changeImage() {
-    imageIndex++;
-    if (imageIndex >= 11) imageIndex = 1;
-    let str = 'url(img/' + imageIndex +'.png);';
-    screenDiv.style.backgroundImage = 'url(img/' + imageIndex +'.png)';
-}
+var tl;
 
 function init() {
-    screenDiv = document.getElementById('screen');
-    const timer = setInterval(changeImage, 2000);
+  screenDiv = document.getElementById('screen');
+  anim();
 }
 
-function fadeOut() {
-    
+function anim() {
+  tl = gsap.timeline().repeat(-1);
+  tl.set(screenDiv, { opacity: 1 },0) 
+  tl.to(screenDiv, { opacity: 0, onComplete: changeImage}, 3) 
+    .to(screenDiv, { opacity: 1, }, 4)
+    .to(screenDiv, { opacity: 1, }, 6)
 }
 
-
+function changeImage() {
+  imageIndex++;
+  if (imageIndex >= 11) imageIndex = 1;
+  let str = 'url(img/' + imageIndex + '.png);';
+  screenDiv.style.backgroundImage = 'url(img/' + imageIndex + '.png)';
+}
 
